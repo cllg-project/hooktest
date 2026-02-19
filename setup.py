838b5c103469b8f7d2b32c93300c7f58c831806a
Thibault Clérice <1929830+PonteIneptique@users.noreply.github.com>
2026-02-19 09:02:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'hooktest=HookTest.cli:cli'
+            'hooktest=hooktest.cli:cli'
         ]
     },
     test_suite="tests",
